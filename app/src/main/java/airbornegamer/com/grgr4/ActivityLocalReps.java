ec01e24634c7200d71a27d9f69b6c544af47c11a
Adam <Adam@AirborneGamer.com>
2015-08-06 00:53:06 -0500

    got json data for reps.  Adding reps to list, working on adapter stuff.

--- app/src/main/java/airbornegamer/com/grgr4/ActivityLocalReps.java
+++ app/src/main/java/airbornegamer/com/grgr4/ActivityLocalReps.java
@@ -1,7 +1,6 @@
 package airbornegamer.com.grgr4;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -9,29 +8,26 @@ import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.ExecutionException;
 
 
 //https://www.govtrack.us/
@@ -40,131 +36,155 @@ import java.util.concurrent.ExecutionException;
 
 //https://www.opencongress.org/
 
+
 public class ActivityLocalReps extends ActionBarActivity {
 
-    private ImageView iv;
-    private Bitmap bitmap;
+    ListView localRepsListView;
+
+    //ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_local_reps);
 
-        //Do all below stuff in long running async task;
+        //Do all below stuff in running async task;
         //http://stackoverflow.com/questions/3090650/android-loading-an-image-from-the-web-with-asynctask
         //http://developer.android.com/intl/ko/reference/android/os/AsyncTask.html
 
+        localRepsListView = (ListView)findViewById(R.id.lstViewYourRepresentatives);
+        ArrayList<String> myArrayList = new ArrayList<String>();
+
         String currentState = getCurrentUsersState();
-        if (currentState != null && PhysicalStateIsKnown(currentState)){
-            SetStateLabel(currentState);
-            SetStateFlag(currentState);
-            setRepresentativeInfo(currentState);
-        }else{
+        if (currentState != null && physicalStateIsKnown(currentState)) {
+            setStateLabel(currentState);
+            setStateFlag(currentState);
+            getRepresentativeInfo(currentState);
+
+            myArrayList = setRepresentativeInfoOntoScreen();
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myArrayList);
+            localRepsListView.setAdapter(adapter);
+
+        } else {
             //State not know, do something (audit...write msg to user....for us user only at this time....Manually select state....)
         }
     }
 
-    public String getCurrentUsersState(){
+    public String getCurrentUsersState() {
 
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
 
-        try{
+        try {
             Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
             List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
             return addresses.get(0).getAdminArea();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             return null;
         }
 
     }
 
-    public Boolean PhysicalStateIsKnown(String currentState){
-        String[] knownStates = getResources().getStringArray(R.array.KnowStates);
+    public Boolean physicalStateIsKnown(String currentState) {
 
-        for(Integer i=0; i < knownStates.length; i++){
+        String[] knownStates = getResources().getStringArray(R.array.KnowStates);
+        for (int i = 0; i < knownStates.length; i++) {
             String[] StatePair = knownStates[i].split(",");
 
             //StatePair[0] is full state name (Nebraska) ; StatePair[1] is state abbreviation (NE).
-            if(StatePair[0].equals(currentState) || StatePair[1].equals(currentState)){
+            if (StatePair[0].equals(currentState) || StatePair[1].equals(currentState)) {
                 return true;
             }
         }
         return false;
     }
 
-    public void SetStateLabel(String currentState){
-        TextView stateLabel = (TextView)findViewById(R.id.txtYourLocation);
+    public void setStateLabel(String currentState) {
+        TextView stateLabel = (TextView) findViewById(R.id.txtYourLocation);
         stateLabel.setText(currentState);
     }
 
-    public void SetStateFlag(String currentState){
+    public void setStateFlag(String currentState) {
 //                Find image of currentState and set below.....
 //                ImageView stateImage = (ImageView)findViewById(R.id.imgYourState);
 //                stateImage.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.flag_ne));
     }
 
-    public void setRepresentativeInfo(String currentState){
-
-        String repInfo = getRepresentativeInfo();
-
+    public void getRepresentativeInfo(String currentState) {
+        String repUrl = buildCustomAPIURL(currentState);
+        new asyncPopulate_allRepData().execute(repUrl);
     }
 
-    public String getRepresentativeInfo(){
-        //https://www.govtrack.us/api/v2/role?state=ne&current=true
-        //parse json
-        // http://stackoverflow.com/questions/2845599/how-do-i-parse-json-from-a-java-httpresponse
+    public String buildCustomAPIURL(String currentState){
+        String firstPartOfURL = "https://www.govtrack.us/api/v2/role?state=";
+        String lastPartOfURL = "&current=true";
 
-        String repUrl = "https://www.govtrack.us/api/v2/role?state=ne&current=true";
-        new DownloadLink().execute(repUrl);
-
-
-       return null;
+        String[] knownStates = getResources().getStringArray(R.array.KnowStates);
+        for(int i = 0; i < knownStates.length; i++){
+            String[] StatePair = knownStates[i].split(",");
+            if (currentState.equals(StatePair[0])){
+                return firstPartOfURL + StatePair[1] + lastPartOfURL;
+            }
+        }
+            return "";
     }
 
-    class DownloadLink extends AsyncTask<String, Void, Void> {
+    JSONObject allRepData = new JSONObject();  //allRepData is populated with below call to a JSON object.
+    class asyncPopulate_allRepData extends AsyncTask<String, Void, Void> {
 
         @Override
         protected Void doInBackground(String... params) {
-            try {
-                URL url = new URL("https://www.govtrack.us/api/v2/role?state=ne&current=true");
-                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            HttpURLConnection urlConnection = null;
+            InputStream inStream = null;
 
-                StringBuilder sb = new StringBuilder();
-                String line;
-                //br = new BufferedReader(new InputStreamReader(new InputStreamReader(url.openStream()));
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String temp, response = "";
+                while ((temp = bReader.readLine()) != null) {
+                    response += temp;
                 }
 
-                JSONObject json = new JSONObject(sb.toString());
-                //json.getString("status");
-                //JSONArray jsonArray = new JSONArray(json.getString("object"));
-                //JSONArray jsonArray = json.getJSONArray("object");
+                allRepData = (JSONObject) new JSONTokener(response).nextValue();
 
-
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 String myEx = ex.toString();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
             }
-
-
             return null;
         }
     }
 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
+    public ArrayList<String> setRepresentativeInfoOntoScreen(){
+        try{
+            JSONArray allRepsResults = allRepData.getJSONArray("objects");
+            ArrayList<String> aList = new ArrayList<String>();
 
-        inputStream.close();
-        return result;
+            for (int i = 0; i < allRepsResults.length(); i++) {
+                JSONObject currentItem = allRepsResults.getJSONObject(i);
+                JSONObject person = currentItem.getJSONObject("person");
 
+                String firstName = person.getString("firstname");
+                String lastName = person.getString("lastname");
+
+                aList.add(firstName + " " + lastName);
+            }
+            return aList;
+        }catch (Exception ex){
+            //trace out bad stuff
+        }
+        return null;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -184,12 +204,8 @@ public class ActivityLocalReps extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-
-
 }
 
 
--- app/src/main/java/airbornegamer/com/grgr4/MainActivity.java
+++ app/src/main/java/airbornegamer/com/grgr4/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         // For each of the sections in the app, add a tab to the action bar.
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
             // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
+            // the adapter. Also specify this Activity allRepData, which implements
             // the TabListener interface, as the callback (listener) for when
             // this tab is selected.
             actionBar.addTab(
